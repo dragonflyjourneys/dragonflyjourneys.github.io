e9bc487d8e548f13a96aec2233abd2f83dba209e
demeteraszabolcs <54589768+demeteraszabolcs@users.noreply.github.com>
2023-06-04 00:59:13 +0300

    jquery navbar, margins

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,75 @@
 /*!
-* Start Bootstrap - Modern Business v5.0.7 (https://startbootstrap.com/template-overviews/modern-business)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-modern-business/blob/master/LICENSE)
-*/
+ * Start Bootstrap - Modern Business v5.0.7 (https://startbootstrap.com/template-overviews/modern-business)
+ * Copyright 2013-2023 Start Bootstrap
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-modern-business/blob/master/LICENSE)
+ */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+$(function () {
+  $("#nav").html(`
+        <nav class="navbar navbar-expand-lg navbar-dark fixed-top bg-dark">
+            <div class="container px-5">
+                <a class="navbar-brand" href="index.html">Dragonfly Journeys</a>
+                <button
+                class="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+                >
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                    <a class="nav-link" href="index.html">Home</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="about.html">About</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                    <a
+                        class="nav-link dropdown-toggle"
+                        id="navbarDropdownBlog"
+                        href="#"
+                        role="button"
+                        data-bs-toggle="dropdown"
+                        aria-expanded="false"
+                        >Transformation</a
+                    >
+                    <ul
+                        class="dropdown-menu dropdown-menu-end"
+                        aria-labelledby="navbarDropdownBlog"
+                    >
+                        <li>
+                        <a class="dropdown-item" href="integration.html"
+                            >Psychedelic Integration</a
+                        >
+                        </li>
+                        <li>
+                        <a class="dropdown-item" href="kambo.html">Kambo Healing</a>
+                        </li>
+                        <li>
+                        <a class="dropdown-item" href="microdosing.html"
+                            >Microdosing Guidance</a
+                        >
+                        </li> 
+                        <li>
+                        <a class="dropdown-item" href="warrior.html"
+                            >Path of the warrior</a
+                        >
+                        </li>
+                    </ul>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="contact.html">Contact</a>
+                    </li>
+                </ul>
+                </div>
+            </div>
+        </nav>
+  `);
+});
