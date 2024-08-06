bc4549e7bea0daf74b0c05ccf294af39eb33c722
dragonflyjourneys <balintlorand@outlook.com>
2024-08-06 01:38:00 +0100

    pss

--- js/scripts.js
+++ js/scripts.js
@@ -69,9 +69,7 @@ $(function () {
                     <li>
                     <a class="dropdown-item" href="kambo.html">Kambo Healing</a>
                     </li>
-                    <li>
-                    <a class="dropdown-item" href="psilohuasca.html">Psilohuasca</a>
-                    </li>
+ 
   
              
 
@@ -104,4 +102,99 @@ $(function () {
             </div>
         </nav>
   `);
+  $("#navacademy").html(`
+  <nav class="navbar navbar-expand-lg navbar-dark fixed-top bg-dark">
+      <div class="container px-5">
+          <a class="navbar-brand" href="index.html">Dragonfly Academy</a>
+          <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+          >
+          <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+              <a class="nav-link" href="index.html">Home</a>
+              </li>
+              <li class="nav-item ">
+              <a
+                  class="nav-link "
+                  href="training.html"
+                  >Trainings</a>
+
+
+              </li>
+              <li class="nav-item ">
+              <a
+                  class="nav-link "
+                  href="education.html"
+                  >Education</a>
+
+
+              </li>
+
+        
+              <li class="nav-item dropdown">
+              <a
+                  class="nav-link dropdown-toggle"
+                  id="navbarDropdownBlog"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                  >Transformation</a
+              >
+              <ul
+                  class="dropdown-menu dropdown-menu-end"
+                  aria-labelledby="navbarDropdownBlog"
+              >
+              <li>
+              <a class="dropdown-item" href="preparation.html">Journey Preparation</a>
+              </li>
+              <li>
+              <a class="dropdown-item" href="microdosing.html"
+                  >Microdosing Guidance</a>
+              </li> 
+              <li>
+              <a class="dropdown-item" href="kambo.html">Kambo Healing</a>
+              </li>
+
+
+       
+
+         
+                  <li>
+                  <a class="dropdown-item" href="assistance.html"
+                      >Trip Crisis Assistance</a
+                  >
+                  </li>
+                  <li>
+                  <a class="dropdown-item" href="warrior.html"
+                    >Path of the Warrior</a
+                  >
+                </li>
+              </ul>
+              </li>
+     
+              <li class="nav-item">
+              <a class="nav-link" href="feedback.html">Testimonials</a>
+              </li>
+              <li class="nav-item">
+              <a class="nav-link" href="about.html">About</a>
+              </li>
+
+              <li class="nav-item">
+              <a class="nav-link" href="contact.html">Contact</a>
+              </li>
+          </ul>
+          </div>
+      </div>
+  </nav>
+`);
 });
